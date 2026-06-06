e1dafbb940293b8475f65a27c0b19b2a53da3b89
dbdlf <yujeongdlek@gmail.com>
2026-06-07 00:56:49 +0900

    Delete frontend directory

--- frontend/src/api/index.js
+++
@@ -1,9 +0,0 @@
-export const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-export * from './authApi';
-export * from './itemApi';
-export * from './matchApi';
-export * from './profileApi';
-export * from './chatApi';
-export * from './matchApi';
-export * from './profileApi';
